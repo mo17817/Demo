ab0c5cf922f42cebbfc3783488d9cd34dfd14d70
mo17817 <lazerharry@gmail.com>
2021-10-23 18:16:40 -0400

    demo repo

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <parralax></parralax>
+  </div>
+</template>
+
+<script>
+import navbar from 'src/components/navbar.vue';
+import Parralax from 'src/components/Parralax.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { Parralax }, { navbar },
+  name: 'PageIndex'
+})
+</script>
